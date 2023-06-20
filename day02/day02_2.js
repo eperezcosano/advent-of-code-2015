a007dfff36c10fd97f6e9241dc2842038e921895
Ethan <izanperez98@gmail.com>
2023-06-20 13:31:30 +0200

    day02 init

---
+++ day02/day02_2.js
@@ -0,0 +1,16 @@
+/*
+*   --- Day 2: I Was Told There Would Be No Math ---
+*                  --- Part One ---
+*                Advent Of Code 2015
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day02.txt')
+})
+
+lineReader.on('line', (line) => {
+})
+
+lineReader.on('close', () => {
+    // Total:
+})
