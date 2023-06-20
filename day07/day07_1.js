54aa007f7b8136b6ad9af5f0d641d60d0c67e593
Ethan <izanperez98@gmail.com>
2023-06-20 17:49:48 +0200

    day07 init

---
+++ day07/day07_1.js
@@ -0,0 +1,17 @@
+/*
+*   --- Day 7: Some Assembly Required ---
+*             --- Part One ---
+*           Advent Of Code 2015
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./test.txt')
+})
+
+lineReader.on('line', (line) => {
+})
+
+lineReader.on('close', () => {
+    // Total:
+})
+
