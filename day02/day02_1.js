8ad6cbe6019e7d6c442587db2488ea1157e657f3
Ethan <izanperez98@gmail.com>
2023-06-20 14:19:06 +0200

    day02 part 1

--- day02/day02_1.js
+++ day02/day02_1.js
@@ -8,10 +8,17 @@ const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('./day02.txt')
 })
 
+let sum = 0
+
 lineReader.on('line', (line) => {
+    const box = line.split('x').map(item => parseInt(item))
+    const surface = 2 * (box[0] * box[1] + box[1] * box[2] + box[2] * box[0])
+    box.sort((a, b) => a - b)
+    const slack = box[0] * box[1]
+    sum += surface + slack
 })
 
 lineReader.on('close', () => {
-    // Total:
+    console.log('Total:', sum)
+    // Total: 1606483
 })
-
