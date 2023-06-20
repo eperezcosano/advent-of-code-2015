68ee998a4bbb34f16f2b43cd89c8ff2411307532
Ethan <izanperez98@gmail.com>
2023-06-20 17:36:42 +0200

    day06 part two

--- day06/day06_2.js
+++ day06/day06_2.js
@@ -1,18 +1,42 @@
-
 /*
-*   --- Day X:  ---
-*          --- Part X ---
-*        Advent Of Code 2015
+*   --- Day 6: Probably a Fire Hazard ---
+*             --- Part Two ---
+*           Advent Of Code 2015
 * */
 
 const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('./day06.txt')
 })
 
+const N = 1_000
+const grid = new Array(N).fill(0).map(() => new Array(N).fill(0))
+
+function parseCoords(string) {
+    return string.split(',').map(value => parseInt(value))
+}
+
+function turnLights(state, [fromX, fromY], [toX, toY]) {
+    for (let y = fromY; y <= toY; y++) {
+        for (let x = fromX; x <= toX; x++) {
+            if (state === 'on') grid[y][x]++
+            else if (state === 'off') grid[y][x] > 0 ? grid[y][x]-- : null
+            else if (state === 'toggle') grid[y][x] += 2
+        }
+    }
+}
+
 lineReader.on('line', (line) => {
+    let state, from, to
+    if (line.startsWith('turn')) {
+        [, state, from, , to] = line.split(' ')
+    } else {
+        [state, from, , to] = line.split(' ')
+    }
+    turnLights(state, parseCoords(from), parseCoords(to))
 })
 
 lineReader.on('close', () => {
-    // Total:
+    const sum = grid.reduce((total, row) => total + row.reduce((sum, value) => sum + value, 0), 0)
+    console.log('Total:', sum)
+    // Total: 17836115
 })
-
