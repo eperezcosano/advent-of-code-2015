d8577b9e231bcaa3674c7b4c3693c6ea0bd868a0
Ethan <izanperez98@gmail.com>
2023-06-20 14:49:35 +0200

    day03 part one

--- day03/day03_1.js
+++ day03/day03_1.js
@@ -8,10 +8,24 @@ const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('./day03.txt')
 })
 
+const grid = new Set()
+let steps
+
 lineReader.on('line', (line) => {
+    steps = line.split('')
 })
 
 lineReader.on('close', () => {
-    // Total:
+    let y = 0
+    let x = 0
+    grid.add(`${x}-${y}`)
+    steps.forEach(step => {
+        if (step === '>') x++
+        else if (step === '<') x--
+        else if (step === '^') y--
+        else if (step === 'v') y++
+        grid.add(`${x}-${y}`)
+    })
+    console.log('Total:', grid.size)
+    // Total: 2572
 })
-
