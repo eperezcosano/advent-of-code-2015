9804222f3cb988efdaf239203258795daf63bf53
Ethan <izanperez98@gmail.com>
2023-06-20 16:23:35 +0200

    day05 init

---
+++ day05/day05_1.js
@@ -0,0 +1,20 @@
+/*
+*   --- Day 5: Doesn't He Have Intern-Elves For This? ---
+*                     --- Part One ---
+*                   Advent Of Code 2015
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day05.txt')
+})
+
+let sum = 0
+
+lineReader.on('line', (line) => {
+
+})
+
+lineReader.on('close', () => {
+    // Total:
+})
+
