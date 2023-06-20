5f7866fe3e3f98ba282cf61c1418038e62ffa04e
Ethan <izanperez98@gmail.com>
2023-06-20 15:01:49 +0200

    day04 init

---
+++ day04/day04_2.js
@@ -0,0 +1,17 @@
+/*
+*   --- Day 4: The Ideal Stocking Stuffer ---
+*               --- Part Two ---
+*              Advent Of Code 2015
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day04.txt')
+})
+
+lineReader.on('line', (line) => {
+})
+
+lineReader.on('close', () => {
+    // Total:
+})
+
