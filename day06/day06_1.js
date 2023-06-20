69b35dd9f956e848832a238a1f2d151ed1f2484c
Ethan <izanperez98@gmail.com>
2023-06-20 16:45:39 +0200

    day06 init

---
+++ day06/day06_1.js
@@ -0,0 +1,17 @@
+/*
+*   --- Day 6: Probably a Fire Hazard ---
+*             --- Part One ---
+*           Advent Of Code 2015
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day06.txt')
+})
+
+lineReader.on('line', (line) => {
+})
+
+lineReader.on('close', () => {
+    // Total:
+})
+
