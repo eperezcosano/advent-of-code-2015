d09c1d019d2140c1447d5dd8dd5876427f3d4f78
Ethan <izanperez98@gmail.com>
2023-06-20 12:29:17 +0200

    first commit

--- day01/day1_1.js
+++ day01/day1_1.js
@@ -1,26 +1,16 @@
 /*
-*   --- Day 1: Calorie Counting ---
+*   --- Day 1: Not Quite Lisp ---
 *          --- Part One ---
-*        Advent Of Code 2022
+*        Advent Of Code 2015
 * */
 
 const lineReader = require('readline').createInterface({
-    input: require('fs').createReadStream('./day1_input.txt')
+    input: require('fs').createReadStream('./day1.txt')
 })
 
-let sum = 0
-let calories = []
-
 lineReader.on('line', (line) => {
-    if (!isNaN(parseInt(line))) sum += parseInt(line)
-    else {
-        calories.push(sum)
-        sum = 0
-    }
 })
 
 lineReader.on('close', () => {
-    calories.sort((a, b) => (a > b) ? -1 : 1)
-    console.log('Total:', calories[0])
-    // Total: 70296
+    // Total:
 })
