90930c4aba8be7237ede560c6ed72d4d6a558596
Izan Pérez Cosano <izanperez98@gmail.com>
2023-06-21 01:55:49 +0200

    day07 dev part one

--- day07/day07_1.js
+++ day07/day07_1.js
@@ -5,10 +5,14 @@
 * */
 
 const lineReader = require('readline').createInterface({
-    input: require('fs').createReadStream('./test.txt')
+    input: require('fs').createReadStream('./day07.txt')
 })
 
 const wires = new Map()
+
+function uint16 (n) {
+    return n & 0xFFFF
+}
 
 lineReader.on('line', (line) => {
     const words = line.split(' ')
@@ -17,22 +21,22 @@ lineReader.on('line', (line) => {
         wires.set(words[2], parseInt(words[0]))
     } else if (words.length === 4) {
         // Bitwise complement
-        wires.set(words[3], ~wires.get(words[1]))
+        wires.set(words[3], uint16(~wires.get(words[1])))
     } else {
         // Rest of Bitwise operations
         if (words[1] === 'AND')
-            wires.set(words[4], wires.get(words[0]) & wires.get(words[2]))
+            wires.set(words[4], uint16(wires.get(words[0]) & wires.get(words[2])))
         else if (words[1] === 'OR')
-            wires.set(words[4], wires.get(words[0]) | wires.get(words[2]))
+            wires.set(words[4], uint16(wires.get(words[0]) | wires.get(words[2])))
         else if (words[1] === 'LSHIFT')
-            wires.set(words[4], wires.get(words[0]) << words[2])
+            wires.set(words[4], uint16(wires.get(words[0]) << parseInt(words[2])))
         else if (words[1] === 'RSHIFT')
-            wires.set(words[4], wires.get(words[0]) >>> words[2])
+            wires.set(words[4], uint16(wires.get(words[0]) >> parseInt(words[2])))
     }
 })
 
 lineReader.on('close', () => {
-    console.log(wires.entries())
+    console.log(wires.get('a'))
     // Result:
 })
 
