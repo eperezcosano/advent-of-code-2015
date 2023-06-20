74d4673a564bf2769476165ff439d00787d7a438
Ethan <izanperez98@gmail.com>
2023-06-20 14:26:41 +0200

    day02 part 2

--- day02/day02_2.js
+++ day02/day02_2.js
@@ -1,6 +1,6 @@
 /*
 *   --- Day 2: I Was Told There Would Be No Math ---
-*                  --- Part One ---
+*                  --- Part Two ---
 *                Advent Of Code 2015
 * */
 
@@ -8,9 +8,16 @@ const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('./day02.txt')
 })
 
+let sum = 0
+
 lineReader.on('line', (line) => {
+    const box = line.split('x').map(item => parseInt(item)).sort((a, b) => a - b)
+    const present = 2 * (box[0] + box[1])
+    const bow = box[0] * box[1] * box[2]
+    sum += present + bow
 })
 
 lineReader.on('close', () => {
-    // Total:
+    console.log('Total:', sum)
+    // Total: 1606483
 })
