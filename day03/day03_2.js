418e8c01d76d75ab9b20f4fbe747fc51bdf25741
Ethan <izanperez98@gmail.com>
2023-06-20 14:31:34 +0200

    day03 init

---
+++ day03/day03_2.js
@@ -0,0 +1,17 @@
+/*
+*   --- Day 3: Perfectly Spherical Houses in a Vacuum ---
+*                       --- Part One ---
+*                     Advent Of Code 2015
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day03.txt')
+})
+
+lineReader.on('line', (line) => {
+})
+
+lineReader.on('close', () => {
+    // Total:
+})
+
