f67564ffbb8150aed05c11ce8745936f3d358850
Ethan <izanperez98@gmail.com>
2023-06-22 17:57:23 +0200

    day07 part one

--- day07/day07_1.js
+++ day07/day07_1.js
@@ -6,49 +6,47 @@
 const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('./day07.txt')
 })
+const commandRegex = /[A-Z]+/g;
+const argRegex = /[a-z0-9]+/g;
 
 const wires = new Map()
-
-function not(n) {
-    return ~n & 0xFFFF
+const bitwiseOperations = {
+    AND: (a, b) => a & b,
+    OR: (a, b) => a | b,
+    NOT: a => ~a,
+    LSHIFT: (a, b) => a << b,
+    RSHIFT: (a, b) => a >> b
 }
-
-function lShift(n, m) {
-    return (n << m) & 0xFFFF
-}
-
-function rShift(n, m) {
-    return (n >> m) & 0xFFFF
-}
-
 lineReader.on('line', (line) => {
-    const words = line.split(' ')
-    wires.set(words.pop(), words.slice(0, -1))
+    const command = line.match(commandRegex)
+    const args = line.match(argRegex).map(arg => isNaN(parseInt(arg)) ? arg : parseInt(arg))
+    const destination = args.pop()
+    wires.set(destination, {command, args})
 })
 
-function calculate(gate) {
-    const stm = wires.get(gate)
-    if (!isNaN(stm)) return stm
+function calculate(wireId) {
+    const wire = wires.get(wireId)
 
-    if (stm.length === 1 && isNaN(parseInt(stm[0]))) return calculate(stm[0])
-    if (stm.length === 2 && stm[0] === 'NOT') {
+    // WireId is a number
+    if (typeof wireId === 'number') return wireId
+    // Wire is pointing to a number
+    if (typeof wire === 'number') return wire
+    // WireId is undefined (second arg on not gate)
+    if (typeof wire === 'undefined') return undefined
 
+    if (!wire.command) {
+        // Connected directly to another wire
+        wires.set(wireId, calculate(wire.args[0]))
+    } else {
+        // Calculate operation
+        const res = bitwiseOperations[wire.command](calculate(wire.args[0]), calculate(wire.args[1]))
+        wires.set(wireId, res)
     }
+
+    return wires.get(wireId)
 }
 
-// function calculate(gate) {
-//     if (!isNaN(parseInt(gate))) return parseInt(gate)
-//     const statement = wires.get(gate)
-//     if (statement.length === 1 && isNaN(parseInt(statement[0]))) return calculate(statement[0])
-//     if (statement[0] === 'NOT') return not(calculate(statement[1]))
-//     if (statement[1] === 'AND') return calculate(statement[0]) & calculate(statement[2])
-//     if (statement[1] === 'OR') return calculate(statement[0]) | calculate(statement[2])
-//     if (statement[1] === 'LSHIFT') return lShift(calculate(statement[0]), statement[2])
-//     if (statement[1] === 'RSHIFT') return rShift(calculate(statement[0]), statement[2])
-//     return parseInt(statement[0])
-// }
-
 lineReader.on('close', () => {
-    console.log(calculate('a'))
-    // Result:
+    console.log('Result:', calculate('a'))
+    // Result: 3176
 })
--- day07/day07_2.js
+++ day07/day07_2.js
@@ -1,18 +1,52 @@
-
 /*
-*   --- Day X:  ---
-*          --- Part X ---
-*        Advent Of Code 2015
+*   --- Day 7: Some Assembly Required ---
+*             --- Part Two ---
+*           Advent Of Code 2015
 * */
-
 const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('./day07.txt')
 })
+const commandRegex = /[A-Z]+/g;
+const argRegex = /[a-z0-9]+/g;
 
+const wires = new Map()
+const bitwiseOperations = {
+    AND: (a, b) => a & b,
+    OR: (a, b) => a | b,
+    NOT: a => ~a,
+    LSHIFT: (a, b) => a << b,
+    RSHIFT: (a, b) => a >> b
+}
 lineReader.on('line', (line) => {
+    const command = line.match(commandRegex)
+    const args = line.match(argRegex).map(arg => isNaN(parseInt(arg)) ? arg : parseInt(arg))
+    const destination = args.pop()
+    wires.set(destination, {command, args})
 })
+
+function calculate(wireId) {
+    const wire = wires.get(wireId)
+
+    // WireId is a number
+    if (typeof wireId === 'number') return wireId
+    // Wire is pointing to a number
+    if (typeof wire === 'number') return wire
+    // WireId is undefined (second arg on not gate)
+    if (typeof wire === 'undefined') return undefined
+
+    if (!wire.command) {
+        // Connected directly to another wire
+        wires.set(wireId, calculate(wire.args[0]))
+    } else {
+        // Calculate operation
+        const res = bitwiseOperations[wire.command](calculate(wire.args[0]), calculate(wire.args[1]))
+        wires.set(wireId, res)
+    }
+
+    return wires.get(wireId)
+}
 
 lineReader.on('close', () => {
-    // Total:
+    console.log('Result:', calculate('a'))
+    // Result: 3176
 })
-
