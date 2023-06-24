9256ddc61710e7c1077b647d078c6625e645d75d
Izan Pérez Cosano <izanperez98@gmail.com>
2023-06-25 01:44:18 +0200

    day08 dev part two

--- day08/day08_2.js
+++ day08/day08_2.js
@@ -1,18 +1,34 @@
-
 /*
-*   --- Day X:  ---
-*          --- Part X ---
-*        Advent Of Code 2015
+*   --- Day 8: Matchsticks ---
+*        --- Part Two ---
+*      Advent Of Code 2015
 * */
 
 const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('./day08.txt')
 })
 
+let encoded = 0
+let original = 0
+
+function count(string) {
+    let i = 0
+    let count = 0
+    let char
+    while (char = string[i++]) {
+        if (char === '"' || char === '\\') count += 2
+        count++
+    }
+    return count
+}
+
 lineReader.on('line', (line) => {
+    original += line.length
+    encoded += count(line)
 })
 
 lineReader.on('close', () => {
-    // Total:
+    console.log('Result:', encoded - original)
+    // Total: 1371
 })
 
