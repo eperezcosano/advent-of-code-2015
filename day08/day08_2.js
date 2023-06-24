ed3a1a5e7237fd0aa76994b8fd8c58fff88c51bf
Izan Pérez Cosano <izanperez98@gmail.com>
2023-06-24 23:58:31 +0200

    day08 init

---
+++ day08/day08_2.js
@@ -0,0 +1,18 @@
+
+/*
+*   --- Day X:  ---
+*          --- Part X ---
+*        Advent Of Code 2015
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day08.txt')
+})
+
+lineReader.on('line', (line) => {
+})
+
+lineReader.on('close', () => {
+    // Total:
+})
+
