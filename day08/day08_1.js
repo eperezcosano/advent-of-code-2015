dc2b1dff8cc3beda3ed7b641decd95802faa6179
Izan Pérez Cosano <izanperez98@gmail.com>
2023-06-25 01:30:56 +0200

    day08 dev part one

--- day08/day08_1.js
+++ day08/day08_1.js
@@ -11,46 +11,28 @@ const lineReader = require('readline').createInterface({
 let code = 0
 let char = 0
 
-function getASCII(hex) {
-    return String.fromCharCode(parseInt(hex, 16))
-}
-function remove(string, from, to) {
-    return string.substring(0, from) + string.substring(to);
-}
-
-function insert(string, char, position) {
-    return [string.slice(0, position), char, string.slice(position)].join('')
+function count(string) {
+    let i = 0
+    let count = 0
+    let char
+    while (char = string[i++]) {
+        if (char === '"') continue
+        if (char === '\\') {
+            if (string[i] === 'x') i += 2
+            i++
+        }
+        count++
+    }
+    return count
 }
 
 lineReader.on('line', (line) => {
     code += line.length
-
-    line = line.slice(1)
-
-    while (line.indexOf('\\x') > -1) {
-        const asciiPos = line.indexOf('\\x')
-        const ascii = getASCII(line.substring(asciiPos + 2, asciiPos + 4))
-        line = remove(line, asciiPos, asciiPos + 4)
-        line = insert(line, ascii, asciiPos)
-    }
-
-    while (line.indexOf('\\"') > -1) {
-        const pos = line.indexOf('\\"')
-        line = remove(line, pos, pos + 1)
-    }
-
-    while (line.indexOf('\\\\') > -1) {
-        const pos = line.indexOf('\\\\')
-        line = remove(line, pos, pos + 1)
-    }
-
-    line = line.slice(0, -1)
-
-    char += line.length
+    char += count(line)
 })
 
 lineReader.on('close', () => {
-    console.log(code, char, code - char)
-    // Total:
+    console.log('Result:', code - char)
+    // Total: 1371
 })
 
