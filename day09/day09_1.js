8b4a0410fd819b2bc041ddc34394d4ed18931dc1
Ethan <izanperez98@gmail.com>
2023-06-26 17:15:34 +0200

    day9 init

---
+++ day09/day09_1.js
@@ -0,0 +1,17 @@
+/*
+*   --- Day 9: All in a Single Night ---
+*            --- Part One ---
+*          Advent Of Code 2015
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day09.txt')
+})
+
+lineReader.on('line', (line) => {
+})
+
+lineReader.on('close', () => {
+    // Total:
+})
+
