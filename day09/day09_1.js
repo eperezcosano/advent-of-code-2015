56720af4612195bf34e66ca3d165961ed7febc67
Ethan <izanperez98@gmail.com>
2023-06-26 17:26:54 +0200

    day9 dev part one

--- day09/day09_1.js
+++ day09/day09_1.js
@@ -8,10 +8,14 @@ const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('./day09.txt')
 })
 
+const graph = new Map()
+
 lineReader.on('line', (line) => {
+    const [from, , to, , cost] = line.split(' ')
+    graph.set([from, to].join('-'), parseInt(cost))
 })
 
 lineReader.on('close', () => {
-    // Total:
+    console.log(graph)
 })
 
