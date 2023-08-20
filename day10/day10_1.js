3eb2d60aa411352d682e2e6f7682b19193c3a60a
Ethan <izanperez98@gmail.com>
2023-08-20 14:33:39 +0200

    day10 part one

--- day10/day10_1.js
+++ day10/day10_1.js
@@ -9,9 +9,37 @@ const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('./day10.txt')
 })
 
+let input
+
+function lookAndSay(input) {
+    const numberStr = input.toString()
+    let res = ""
+    for(let i = 0; i < numberStr.length; i++) {
+        const digit = numberStr[i]
+        for(let j = i, count = 0; j < numberStr.length; j++) {
+            if (digit === numberStr[j]) {
+                count++
+                if (j === numberStr.length - 1) {
+                    return res.concat(count.toString().concat(digit))
+                }
+            }
+            else {
+                res += count.toString().concat(digit)
+                i = j - 1
+                break
+            }
+        }
+    }
+}
+
 lineReader.on('line', (line) => {
+    input = line
 })
 
 lineReader.on('close', () => {
-    // Total:
+    for(let i = 0; i < 40; i++) {
+        input = lookAndSay(input)
+    }
+    console.log('Result:', input.length)
+    // Result: 252594
 })
