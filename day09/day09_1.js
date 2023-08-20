c3e9d29f72e1c34b99bf66bdf5aee65d669e5d49
Ethan <izanperez98@gmail.com>
2023-08-20 13:22:40 +0200

    day09 part one

--- day09/day09_1.js
+++ day09/day09_1.js
@@ -8,14 +8,53 @@ const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('./day09.txt')
 })
 
-const graph = new Map()
+const map = new Map()
+const places = new Set()
+
+function permute(input) {
+
+    function permuteHelper(res, item, key, arr) {
+        if (arr.length > 1) {
+            const remainingItems = arr.slice(0, key).concat(arr.slice(key + 1))
+            const permutations = remainingItems.reduce(permuteHelper, [])
+            const itemPermutations = permutations.map(perm => [item].concat(perm))
+            return res.concat(itemPermutations)
+        } else {
+            return res.concat(item)
+        }
+    }
+
+    const array = Array.from(input)
+    return array.reduce(permuteHelper, [])
+}
+
+function calculateCosts(allPermutations) {
+    const costs = []
+    for(const route of allPermutations) {
+        let cost = 0
+        for(let i = 0; i < route.length - 1; i++) {
+            const pair = [route[i], route[i + 1]]
+            if (map.has(pair.join('-'))) {
+                cost += map.get(pair.join('-'))
+            } else {
+                cost += map.get(pair.reverse().join('-'))
+            }
+        }
+        costs.push(cost)
+    }
+    return costs
+}
 
 lineReader.on('line', (line) => {
     const [from, , to, , cost] = line.split(' ')
-    graph.set([from, to].join('-'), parseInt(cost))
+    map.set([from, to].join('-'), parseInt(cost))
+    places.add(from).add(to)
 })
 
 lineReader.on('close', () => {
-    console.log(graph)
+    const allPermutations = permute(places)
+    const costs = calculateCosts(allPermutations)
+    console.log('Result:', Math.min(...costs))
+    // Result: 141
 })
 
