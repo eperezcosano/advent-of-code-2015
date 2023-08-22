d5ba700bb8b0c6d6139323dce032e883f0c986f8
Ethan <izanperez98@gmail.com>
2023-08-22 18:18:13 +0200

    dev day12 part two

--- day12/day12_2.js
+++ day12/day12_2.js
@@ -1,17 +1,36 @@
 /*
-*   --- Day X:  ---
-*          --- Part X ---
-*        Advent Of Code 2015
+*   --- Day 12: JSAbacusFramework.io ---
+*             --- Part Two ---
+*            Advent Of Code 2015
 * */
 
 const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('./day12.txt')
 })
 
-lineReader.on('line', (line) => {
-})
+let doc
+
+function findSum() {
+    return doc.match(/-?\d+/g).reduce((acc, val) => acc + parseInt(val), 0)
+}
+
+function excludeRed() {
+    let sum = 0
+    const json = JSON.parse(doc)
+
+    for (const key of json) {
+        if (typeof ) {}
+        
+    }
+
+    //if (typeof value === "number") sum += parseInt(value)
+    console.log(sum)
+}
+
+lineReader.on('line', line => doc = line)
 
 lineReader.on('close', () => {
-    // Result:
+    excludeRed()
+    //console.log('Result:', findSum())
+    // Result: 156366
 })
-
