e05fa3f9a0acfa515050a73dd15a59830bf91be4
Ethan <izanperez98@gmail.com>
2023-08-23 12:16:39 +0200

    day13 init

---
+++ day13/day13_1.js
@@ -0,0 +1,16 @@
+/*
+*   --- Day 13: Knights of the Dinner Table ---
+*                 --- Part One ---
+*                Advent Of Code 2015
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day13.txt')
+})
+
+lineReader.on('line', (line) => {
+})
+
+lineReader.on('close', () => {
+    // Result:
+})
---
+++ day13/day13_2.js
@@ -0,0 +1,17 @@
+/*
+*   --- Day X:  ---
+*          --- Part X ---
+*        Advent Of Code 2015
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day13.txt')
+})
+
+lineReader.on('line', (line) => {
+})
+
+lineReader.on('close', () => {
+    // Result:
+})
+
