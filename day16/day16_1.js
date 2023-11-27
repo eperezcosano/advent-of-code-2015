f297d203b87840fe7ac491920d52c085fa1e23f1
Ethan <izanperez98@gmail.com>
2023-11-27 13:44:05 +0100

    day16 init

---
+++ day16/day16_1.js
@@ -0,0 +1,16 @@
+/*
+*   --- Day 16: Aunt Sue ---
+*       --- Part One ---
+*     Advent Of Code 2015
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day16.txt')
+})
+
+lineReader.on('line', (line) => {
+})
+
+lineReader.on('close', () => {
+    // Result:
+})
