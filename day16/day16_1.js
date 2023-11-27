10cfcaaa94b7a5b7553ac9071cd90536eade79e6
Ethan <izanperez98@gmail.com>
2023-11-27 18:20:00 +0100

    day16 part one

--- day16/day16_1.js
+++ day16/day16_1.js
@@ -8,9 +8,24 @@ const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('./day16.txt')
 })
 
-lineReader.on('line', (line) => {
-})
+const tape = {
+    children: 3,
+    cats: 7,
+    samoyeds: 2,
+    pomeranians: 3,
+    akitas: 0,
+    vizslas: 0,
+    goldfish: 5,
+    trees: 3,
+    cars: 2,
+    perfumes: 1
+}
 
-lineReader.on('close', () => {
-    // Result:
+lineReader.on('line', (line) => {
+    const [, sue, firstName, firstValue, secondName, secondValue, thirdName, thirdValue] = line.split(' ').map((val, i) => i % 2 == 0 ? val.replace(/[:,]/, '') : parseInt(val.replace(/[:,]/, '')))
+    if (tape[firstName] == firstValue && tape[secondName] == secondValue && tape[thirdName] == thirdValue) {
+        console.log('Result:', sue)
+        // Result: 40
+        process.exit(0)
+    }
 })
