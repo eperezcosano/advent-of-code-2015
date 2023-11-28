61ee04578e9e848158ae3971fb03ea5e34c4f473
Ethan <izanperez98@gmail.com>
2023-11-28 12:07:18 +0100

    day17 init

---
+++ day17/day17_1.js
@@ -0,0 +1,16 @@
+/*
+*   --- Day 17: No Such Thing as Too Much ---
+*                --- Part One ---
+*              Advent Of Code 2015
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day17.txt')
+})
+
+lineReader.on('line', (line) => {
+})
+
+lineReader.on('close', () => {
+    // Result:
+})
