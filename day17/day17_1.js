75e4ef552e64af5c4be1242ac25afb32c8883c88
Ethan <izanperez98@gmail.com>
2024-06-11 12:42:48 +0200

    day17 part one

--- day17/day17_1.js
+++ day17/day17_1.js
@@ -8,9 +8,22 @@ const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('./day17.txt')
 })
 
-lineReader.on('line', (line) => {
-})
+const liters = 150
+const boxes = []
+
+lineReader.on('line', line => boxes.push(parseInt(line)))
 
 lineReader.on('close', () => {
-    // Result:
+    const leading = new Array(boxes.length).fill(0).join('')
+    const res = new Array(2 ** boxes.length)
+        .fill(0)
+        .reduce((acc, _, n) => (leading + n.toString(2))
+            .slice(-boxes.length)
+            .split('')
+            .map(Number)
+            .map((val, i) => val * boxes[i])
+            .reduce((acc, val) => acc + val, 0) === liters ? ++acc : acc, 0
+        )
+    console.log('Result:', res)
+    // Result: 4372
 })
