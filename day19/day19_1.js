6e9f5b7a6aa603d746113c5ea740a9c75a0d4a56
Ethan <izanperez98@gmail.com>
2024-06-12 13:24:26 +0200

    day19 part one

--- day19/day19_1.js
+++ day19/day19_1.js
@@ -8,10 +8,29 @@ const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('./day19.txt')
 })
 
-lineReader.on('line', (line) => {
+const replacements = []
+const molecules = new Set()
+let molecule
+
+let lineBreak = false
+lineReader.on('line', line => {
+    if (line.length === 0) {
+        lineBreak = true
+        return
+    }
+    if (lineBreak) molecule = line
+    else replacements.push(line.split(' => '))
 })
 
 lineReader.on('close', () => {
-    // Result:
+    for (const [from, to] of replacements) {
+        for (let i = 0; i < molecule.length; i++) {
+            i = molecule.indexOf(from, i)
+            if (i < 0) break
+            molecules.add(molecule.substring(0, i) + to + molecule.substring(i + from.length))
+        }
+    }
+    console.log('Result:', molecules.size)
+    // Result: 535
 })
 
