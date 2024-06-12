7c18bb82ce53a5bb15d72d7e2bb10a3a37750694
Ethan <izanperez98@gmail.com>
2024-06-12 12:30:42 +0200

    day19 init

---
+++ day19/day19_1.js
@@ -0,0 +1,17 @@
+/*
+*   --- Day 19: Medicine for Rudolph ---
+*             --- Part One ---
+*           Advent Of Code 2015
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day19.txt')
+})
+
+lineReader.on('line', (line) => {
+})
+
+lineReader.on('close', () => {
+    // Result:
+})
+
