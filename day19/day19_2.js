cd5e8179dc0a992060e486b6dc5598a5ccd17a64
Ethan <izanperez98@gmail.com>
2024-06-13 10:56:23 +0200

    day19 part two

--- day19/day19_2.js
+++ day19/day19_2.js
@@ -8,7 +8,6 @@ const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('./day19.txt')
 })
 
-const replacements = []
 let molecule
 
 let lineBreak = false
@@ -18,49 +17,13 @@ lineReader.on('line', line => {
         return
     }
     if (lineBreak) molecule = line
-    else replacements.push(line.split(' => ').reverse())
 })
 
-function getVariants(molecule, from, to) {
-    const variants = new Set()
-    for (let i = 0; i < molecule.length; i++) {
-        i = molecule.indexOf(from, i)
-        if (i < 0) break
-        variants.add(molecule.substring(0, i) + to + molecule.substring(i + from.length))
-    }
-    return Array.from(variants)
-}
-
-function getSubMolecules(molecules, map) {
-    const total = []
-    for (const subMolecule of molecules) {
-        let max = null
-        for (const [to, from] of map) {
-            const sub = getVariants(subMolecule, to, from)
-            if (sub.length === 0) continue
-            if (to.length < max) break
-            max = to.length
-            //console.log(subMolecule, sub)
-            total.push(...sub)
-        }
-    }
-    //const min = Math.min(...total.map(val => val.length))
-    //const tmp = new Set([...total.filter(val => val.length === min)])
-    //console.log(tmp)
-    return [...new Set(total)]
-}
-
-function steps() {
-    const map = new Map(replacements.sort((a, b) => b[0].length - a[0].length))
-    let molecules = [molecule]
-    for (let steps = 1; steps < 1_000_000; steps++) {
-        molecules = getSubMolecules(molecules, map)
-        if (molecules.includes('e')) return steps
-    }
-}
-
 lineReader.on('close', () => {
-    const res = steps()
+    const elements = molecule.match(/[A-Z]/g).length
+    const radonArgon = molecule.match(/(Rn|Ar)/g).length
+    const yttrium = molecule.match(/Y/g).length
+    const res = elements - radonArgon - 2 * yttrium - 1
     console.log('Result:', res)
-    // Result:
+    // Result: 212
 })
