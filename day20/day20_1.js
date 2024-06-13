1a879a35a0b993071b276ddb73aa56d21fee8efe
Ethan <izanperez98@gmail.com>
2024-06-13 16:37:28 +0200

    day20 part one

--- day20/day20_1.js
+++ day20/day20_1.js
@@ -8,9 +8,34 @@ const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('./day20.txt')
 })
 
-lineReader.on('line', line => {
-})
+let input
+
+function getFactors(num) {
+    const isEven = num % 2 === 0
+    const max = Math.sqrt(num)
+    const inc = isEven ? 1 : 2
+    const factors = [1, num]
+
+    for (let factor = isEven ? 2 : 3; factor <= max; factor += inc) {
+        if (num % factor !== 0) continue
+        factors.push(factor)
+        const compliment = num / factor
+        if (compliment !== factor) factors.push(compliment)
+    }
+    return factors
+}
+
+function getHouse() {
+    for (let num = 1; ; num++) {
+        const sum = getFactors(num).reduce((acc, val) => acc + val, 0)
+        if (sum >= input) return num
+    }
+}
+
+lineReader.on('line', line => input = parseInt(line) / 10)
 
 lineReader.on('close', () => {
-    // Result:
+    const res = getHouse()
+    console.log('Result:', res)
+    // Result: 831600
 })
