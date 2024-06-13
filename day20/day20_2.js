15ff13c55424e10086642689a0256061e8d8324c
Ethan <izanperez98@gmail.com>
2024-06-13 13:59:37 +0200

    day20 init

---
+++ day20/day20_2.js
@@ -0,0 +1,16 @@
+/*
+*   --- Day 20: Infinite Elves and Infinite Houses ---
+*                     --- Part Two ---
+*                   Advent Of Code 2015
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day20.txt')
+})
+
+lineReader.on('line', line => {
+})
+
+lineReader.on('close', () => {
+    // Result:
+})
