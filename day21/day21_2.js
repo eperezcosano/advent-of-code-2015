ff96db6ce60995300e0ca789e6849c06dd31b752
Ethan <izanperez98@gmail.com>
2024-06-16 11:07:48 +0200

    day21 init

---
+++ day21/day21_2.js
@@ -0,0 +1,16 @@
+/*
+*   --- Day 21: RPG Simulator 20XX ---
+*           --- Part Two ---
+*          Advent Of Code 2015
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day21.txt')
+})
+
+lineReader.on('line', line => {
+})
+
+lineReader.on('close', () => {
+    // Result:
+})
