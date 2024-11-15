382168e274ae7ca023856d0ced1e536955c5143b
Ethan <izanperez98@gmail.com>
2024-11-15 15:04:57 +0100

    day24 init

---
+++ day24/day24_1.js
@@ -0,0 +1,16 @@
+/*
+*   --- Day 24: It Hangs in the Balance ---
+*               --- Part One ---
+*             Advent Of Code 2015
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day24.txt')
+})
+
+lineReader.on('line', line => {
+})
+
+lineReader.on('close', () => {
+    // Result:
+})
---
+++ day24/day24_2.js
@@ -0,0 +1,17 @@
+/*
+*   --- Day X:  ---
+*          --- Part X ---
+*        Advent Of Code 2015
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day24.txt')
+})
+
+lineReader.on('line', (line) => {
+})
+
+lineReader.on('close', () => {
+    // Result:
+})
+
