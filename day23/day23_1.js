87b911b5f1bc57d28b28172787ced147cc65e1d9
Ethan <izanperez98@gmail.com>
2024-11-15 13:26:20 +0100

    day23 init

---
+++ day23/day23_1.js
@@ -0,0 +1,17 @@
+/*
+*   --- Day 23: Opening the Turing Lock ---
+*               --- Part One ---
+*              Advent Of Code 2015
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day23.txt')
+})
+
+lineReader.on('line', line => {
+})
+
+lineReader.on('close', () => {
+    // Result:
+})
+
