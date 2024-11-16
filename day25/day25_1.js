c577198ad813865213764180edbbe03438d1793d
Ethan <izanperez98@gmail.com>
2024-11-16 15:46:38 +0100

    day25

--- day25/day25_1.js
+++ day25/day25_1.js
@@ -7,9 +7,24 @@ const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('./day25.txt')
 })
 
+let [row, col] = []
+
+function getCode(row, col) {
+    let num = 20151125
+    for (let max = 2; max <= 1e10; max++) {
+        for (let [y, x] = [1, max]; y <= max; [y++, x--]) {
+            num = (num * 252533) % 33554393
+            if (col === y && row === x) return num
+        }
+    }
+}
+
 lineReader.on('line', line => {
+    [row, col] = line.split(' ').map(c => parseInt(c)).filter(c => !isNaN(c))
 })
 
 lineReader.on('close', () => {
-    // Result:
+    const res = getCode(row, col)
+    console.log('Result:', res)
+    // Result: 2650453
 })
